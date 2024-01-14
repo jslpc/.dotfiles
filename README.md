6f9675abf34f55499bf8a31fc2472f74c8e3959d
Jake Sulpice <jake@jakesulpice.com>
2024-01-13 17:04:54 -0800

    Update aliases, gitignore, zshrc, remove nvim

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# .dotfiles for macOS
+# My basic .dotfiles for macOS
 
-This is very much a work in progress, so it's pretty barebones right now. Hopefully I'll add some more configurations with time.  
+This is very much a work in progress, so it's pretty barebones right now. Hopefully, I'll add some more configurations with time.  
--- zsh/oh-my-zsh-custom/aliases.zsh
+++ zsh/oh-my-zsh-custom/aliases.zsh
@@ -2,19 +2,20 @@
 alias cat='bat'
 
 # General exa
-alias ls='exa'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGd --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-alias lr='exa -als mod'        	                                       #`ls -hartl`
-
-# exa views
-alias lS='exa -1'                                                      # one column, just names
-alias lt='exa --tree --level=2'                                        # tree
+alias ls='exa'      # ls
+alias l='exa -lbF --git'      # list, size, type, git
+alias ll='exa -lbGF --git'      # long list
+alias llm='exa -lbGd --git --sort=modified'     # long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'     # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale'      # all + extended list
+alias lr='exa -als mod'     #`ls -hartl`
+alias lS='exa -1'     # one column, just names
+alias lt='exa --tree --level=2'     # tree
 
 # Avoid accidental deletion
 alias rm='rm -i'
 alias mv='mv -i'
 alias cp='cp -i'
+
+# aria2c
+alias suck='aria2c --file-allocation=none -c -x 10 -s 10 -d "$HOME/Downloads" '
