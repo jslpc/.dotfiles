60b7d1dde0e14ac25db3c8cf2db4c20666e587c5
Jake Sulpice <jake@jakesulpice.com>
2024-08-04 23:05:24 -0700

    Update stuff

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # My basic .dotfiles for macOS
 
-This is very much a work in progress, so it's pretty barebones right now. Hopefully, I'll add some more configurations with time.  
+This is very much a work in progress, so it's pretty barebones right now. I'll add some more configurations with time maybe.
+
+Some folders may or may not have their own README.md files. [Here's the one for beets](beets/README.md).
--- zsh/oh-my-zsh-custom/aliases.zsh
+++ zsh/oh-my-zsh-custom/aliases.zsh
@@ -6,11 +6,9 @@ alias rm='rm -i'
 alias mv='mv -i'
 alias cp='cp -i'
 
-# aria2c downloader
-alias aria='aria2c --file-allocation=none -c -x 10 -s 10 -d "$HOME/Downloads" '
-
-# yt-dlp audio
-alias yt-audio='yt-dlp --config-location ~/.config/yt-dlp/yt-dlp-audio.conf'
+# Shortcuts
+alias aria='aria2c --file-allocation=none -c -x 10 -s 10 -d "$HOME/Downloads" '      # aria2c
+alias yt-audio='yt-dlp --config-location ~/.config/yt-dlp/yt-dlp-audio.conf'            # yt-dlp audio
 
 # Streamrip
 alias srip='~/.pyenv/versions/3.12.2/bin/rip'
