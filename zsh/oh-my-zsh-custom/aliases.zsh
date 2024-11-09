6cd965474dfce25f69b28f51c390578e42b42223
Jake Sulpice <jake@jakesulpice.com>
2024-11-09 00:42:02 -0800

    Updates and such

--- zsh/oh-my-zsh-custom/aliases.zsh
+++ zsh/oh-my-zsh-custom/aliases.zsh
@@ -9,6 +9,7 @@ alias cp='cp -i'
 # Shortcuts
 alias aria='aria2c --file-allocation=none -c -x 10 -s 10 -d "$HOME/Downloads" '      # aria2c
 alias yt-audio='yt-dlp --config-location ~/.config/yt-dlp/yt-dlp-audio.conf'            # yt-dlp audio
+alias yt-dlp='yt-dlp --config-location ~/.config/yt-dlp/yt-dlp.conf'                      # yt-dlp video 
 
 # Streamrip
 alias srip='~/.pyenv/versions/3.12.2/bin/rip'
